00334fd944a9c5a9c7ed463f4629075d1b24441f
Egor Grishechko <egorgrishechko@gmail.com>
2017-11-08 21:22:07 +0300

    refactoring. tests

--- test/BonumFinancialToken.js
+++ test/BonumFinancialToken.js
@@ -1,5 +1,7 @@
 const BonumFinancialToken = artifacts.require("BonumFinancialToken");
-const assertJump = require("zeppelin-solidity/test/helpers/assertJump.js");
+const expect = require('chai').use(require('chai-as-promised')).expect;
+require('chai').use(require('chai-as-promised')).should();
+
 
 contract('BonumFinancialToken', function(accounts) {
     it("should create 75000000 BFT", async function () {
@@ -12,5 +14,31 @@ contract('BonumFinancialToken', function(accounts) {
         const instance = await BonumFinancialToken.new();
         const balance = await instance.balanceOf(accounts[0]);
         assert.equal(balance.valueOf(), 75000000 * 10 ** 18, "Balance must be 75000000");
+    });
+
+    it("shouldn't give an opportunity to call setReleaseAgent for another persons", async function () {
+        const instance = await BonumFinancialToken.new();
+        instance.setReleaseAgent(accounts[1], {from: accounts[1]}).then(
+            function () {
+                assert.fail();
+            },
+            function (err) {
+                err.message.should.includes("VM Exception while processing transaction: revert");
+            }
+        );
+    });
+
+    it("shouldn't allow to set releaseAgent by owner when token is released", async function () {
+        const instance = await BonumFinancialToken.new();
+        await instance.setReleaseAgent(accounts[0]);
+        await instance.releaseTokens();
+        instance.setReleaseAgent(accounts[1], {from: accounts[1]}).then(
+            function () {
+                assert.fail();
+            },
+            function (err) {
+                err.message.should.includes("VM Exception while processing transaction: revert");
+            }
+        );
     });
 });
